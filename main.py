0a2c766a6197657ba9d7920ea9acd3b951b5ed16
Thuandang297 <Thuandang729@gmail.com>
2024-01-11 23:17:24 +0700

    update

--- main.py
+++ main.py
@@ -144,8 +144,8 @@ finally:
  
     #Close down curses properly, inc turn echo back on!
  
-    #curses.nocbreak();
-    #screen.keypad(0);
-    #curses.echo()
-    #curses.endwin()
+    curses.nocbreak();
+    screen.keypad(0);
+    curses.echo()
+    curses.endwin()
     GPIO.cleanup()
